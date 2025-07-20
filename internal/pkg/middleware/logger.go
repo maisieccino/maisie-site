3206adf58697e35dade369d4533db2369f926b92
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-07-20 09:40:41 +0100

    Comments

--- internal/pkg/middleware/logger.go
+++ internal/pkg/middleware/logger.go
@@ -7,6 +7,7 @@ import (
 	"go.uber.org/zap"
 )
 
+// NewLoggerMiddleware adds a log line to requests that the server handles.
 func NewLoggerMiddleware(logger *zap.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
