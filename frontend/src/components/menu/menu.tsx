02f37a0c68620f4fd4675fc4db361059919ae5b1
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-08-23 15:16:09 +0100

    Add design system (#5)
    * Add colors
    
    * begin writing menu

---
+++ frontend/src/components/menu/menu.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./menu.module.css";
+import buttonStyles from "../button/button.module.css";
+
+export interface Props {
+  children: React.ReactNode;
+}
+export const Menu = ({ children }: Props) => (
+  <>
+    <button className={[buttonStyles.buttonShared, styles.menuButton].join(" ")}>Menu</button>
+    <nav>
+      {children}
+    </nav>
+  </>
+)
