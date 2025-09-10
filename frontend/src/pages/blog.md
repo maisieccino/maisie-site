c6a3d9e9cc3a1a433c9e72c6620703da83bffbc0
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-09-10 22:00:54 +0100

    Add support for ghost

--- frontend/src/pages/blog.md
+++
@@ -1,9 +0,0 @@
----
-layout: ../layouts/md.astro
-title: Blog
----
-Eventually this will show my recent blog posts.
-
-Until that happens, here's a link to it.
-
-[Maisie's blog](https://blog.mbell.dev)
---
+++ frontend/src/util/ghost.ts
@@ -0,0 +1,15 @@
+import GhostContentAPI from "@tryghost/content-api";
+const ghostClient = new GhostContentAPI({
+  url: 'https://bell-blog.ghost.io',
+  key: import.meta.env.CONTENT_API_KEY,
+  version: 'v6.0',
+});
+
+export const posts = await ghostClient.posts
+  .browse({
+    limit: 5,
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+
