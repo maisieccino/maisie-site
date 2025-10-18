1d4701b8914d3a944e090c2236404cfa0b3f92b9
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-10-18 23:10:48 +0100

    Importing blog into astro

--- frontend/src/util/ghost.ts
+++ frontend/src/util/ghost.ts
@@ -1,15 +1,31 @@
-import GhostContentAPI, { type PostsOrPages } from "@tryghost/content-api";
+import GhostContentAPI, { type PostOrPage, type PostsOrPages } from "@tryghost/content-api";
+import rehypeParse from "rehype-parse";
+import rehypeRemark from "rehype-remark";
+import remarkStringify from "remark-stringify";
+import { unified } from "unified";
+import astroConfig from "../../astro.config.mjs";
 const ghostClient = new GhostContentAPI({
   url: 'https://bell-blog.ghost.io',
   key: import.meta.env.CONTENT_API_KEY,
   version: 'v6.0',
 });
+astroConfig.env
+
+const limit = import.meta.env.MODE === "development" ? 5 : 100
 
 export const posts: void | PostsOrPages = await ghostClient.posts
   .browse({
-    limit: 5,
+    limit: limit,
   })
   .catch((err) => {
     console.error(err);
   })
 
+
+// Takes a Ghost post and converts to Markdown.
+export const postToMD = async (post: PostOrPage) =>
+  unified()
+    .use(rehypeParse)
+    .use(rehypeRemark)
+    .use(remarkStringify)
+    .process(post.html || "")
