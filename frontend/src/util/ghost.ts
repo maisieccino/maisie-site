0be156acdfee705148da3149c6d9535531653670
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-10-19 13:41:56 +0100

    Tag pages

--- frontend/src/util/ghost.ts
+++ frontend/src/util/ghost.ts
@@ -1,4 +1,4 @@
-import GhostContentAPI, { type PostOrPage, type PostsOrPages } from "@tryghost/content-api";
+import GhostContentAPI, { type PostOrPage, type PostsOrPages, type Tag } from "@tryghost/content-api";
 import rehypeParse from "rehype-parse";
 import rehypeRemark from "rehype-remark";
 import remarkStringify from "remark-stringify";
@@ -15,12 +15,22 @@ const limit = import.meta.env.MODE === "development" ? 50 : 100
 
 export const posts: void | PostsOrPages = await ghostClient.posts
   .browse({
+    include: ["authors", "tags"],
     limit: limit,
   })
   .catch((err) => {
     console.error(err);
   })
 
+
+export const tags = await ghostClient.tags
+  .browse({
+    include: "count.posts",
+    limit: 100,
+  })
+  .catch(err => {
+    console.error(err)
+  })
 
 // Takes a Ghost post and converts to Markdown.
 export const postToMD = async (post: PostOrPage) =>
