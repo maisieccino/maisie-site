9b1772fa9f3ea24a6efe256a30e3ab88e6e7a840
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2026-01-13 23:00:15 +0000

    Create post collection!

--- frontend/src/content.config.ts
+++ frontend/src/content.config.ts
@@ -1,5 +1,56 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { glob, type Loader } from "astro/loaders";
+import { posts as blogPosts, postToMD } from "./util/ghost";
+import { parseISO } from "date-fns";
+import { createMarkdownProcessor } from "@astrojs/markdown-remark";
+
+const globLoader = glob({ pattern: "**/*.md", base: "./src/posts" })
+
+const postLoader: Loader = {
+  name: "post-loader",
+  async load(context) {
+    await globLoader.load(context)
+    const { store, parseData, logger, renderMarkdown } = context
+
+    await Promise.all(
+      (blogPosts || [])
+        .map(post => (
+          {
+            post,
+            data: {
+              id: post.slug,
+              slug: post.slug,
+              title: post.title || "",
+              publishedTime: parseISO(post.published_at || ""),
+              modifiedTime: parseISO(post.updated_at || ""),
+              authors: (post.authors || []).map(a => a.name),
+              tags: (post.tags || []).map(t => t.name),
+              excerpt: post.custom_excerpt || post.excerpt || "",
+              featureImage: post.feature_image || "",
+              featureImageAlt: post.feature_image_alt || "",
+              rendered: post.html || "",
+            }
+          }))
+        .map(async post => {
+          if (post.data.slug === "") {
+            logger.warn(`No slug for post ${post.data.id}`)
+            return
+          }
+          if (store.has(post.data.slug)) {
+            return
+          }
+          const data = await parseData({ id: post.data.slug, data: post.data })
+          logger.debug(`id is ${data.id}`)
+          const md = await postToMD(post.post)
+          store.set({
+            id: post.data.slug,
+            data,
+            rendered: await renderMarkdown(String(md))
+          })
+        })
+    )
+  },
+}
 
 const places = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/places" }),
@@ -13,16 +64,14 @@ const places = defineCollection({
     image: z.string().optional()
   })
 })
-
 const posts = defineCollection({
   // TODO: https://docs.astro.build/en/guides/content-collections/#building-a-custom-loader
-  loader: glob({ pattern: "**/*.md", base: "./src/places" }),
+  loader: postLoader,
   schema: z.object({
-    slug: z.string(),
     title: z.string(),
     publishedTime: z.date(),
     modifiedTime: z.date(),
-    authors: z.string().default("Maisie Bell"),
+    authors: z.array(z.string()).default(["Maisie Bell"]),
     tags: z.array(z.string()),
     excerpt: z.string().optional(),
     featureImage: z.string().optional(),
@@ -31,3 +80,4 @@ const posts = defineCollection({
 })
 
 export const collections = { places, posts }
+
---
+++ frontend/src/posts/test.md
@@ -0,0 +1,9 @@
+---
+title: "Test"
+publishedTime: 2026-01-13T22:37:30
+modifiedTime: 2026-01-13T22:37:39
+tags:
+  - test
+---
+
+Hi
