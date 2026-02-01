24722ad667a442b3178c6c9a8f8971767917304b
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2026-02-01 23:51:32 +0000

    Update rss feed

--- frontend/src/pages/feed.xml.ts
+++ frontend/src/pages/feed.xml.ts
@@ -1,44 +1,41 @@
-import rss, { type RSSFeedItem } from "@astrojs/rss";
-import { posts as blogPosts, postToMD } from "../util/ghost"
+import rss from "@astrojs/rss";
 import type { APIRoute } from "astro";
-import type { PostOrPage } from "@tryghost/content-api";
-import { createMarkdownProcessor } from "@astrojs/markdown-remark";
-import { parseISO } from "date-fns";
+import { getImage } from "astro:assets";
+import { getCollection } from "astro:content";
 
 export const GET: APIRoute = async (context) => {
-  const proc = await createMarkdownProcessor()
-
-  const postToFeedItem = async (post: PostOrPage): Promise<RSSFeedItem> => {
-    const md = await postToMD(post)
-
-    const render = await proc.render(String(md))
-
-    return {
-      author: (post.authors || []).map(a => a.name).join(","),
-      content: render.code,
-      categories: (post.tags || []).map(t => t.name || ""),
-      link: `/post/${post.slug}`,
-      title: post.title || "",
-      pubDate: parseISO(post.published_at || ""),
-      enclosure: {
-        type: "image",
-        url: post.feature_image || "",
-        length: 0,
-      },
-    }
-  }
+  const posts = await getCollection("posts")
 
   return rss({
     title: "Maisieccino",
     description: "Sharing longer thoughts & stories about tech, gender, health, climbing",
     site: context.site || "",
-    items: await Promise.all(
-      (blogPosts || [])
-        .filter(isPost)
-        .map(postToFeedItem)
-    ),
+    items: await Promise.all(posts.sort((a, b) => b.data.publishedTime.valueOf() - a.data.publishedTime.valueOf())
+      .map(async post => ({
+        title: post.data.title,
+        pubDate: post.data.publishedTime,
+        description: post.data.excerpt || "",
+        link: `/post/${post.id}/`,
+        enclosure: {
+          type: "image",
+          url: (await featureImage(post.data.featureImage || "")).default.src,
+          length: 0
+        }
+      }))),
   })
 }
-const isPost = (p: any): p is PostOrPage => {
-  return p.uuid !== undefined;
+
+const featureImage = async (url: string) => {
+  if (url === "") {
+    return { default: { src: url } }
+  }
+  if (URL.parse(url)?.host === "") {
+    return { default: { src: url } }
+  }
+
+  const images = import.meta.glob<{ default: ImageMetadata }>('/src/assets/img/**/*.{jpeg,jpg,png,gif}')
+  if (!images[url]) {
+    return { default: { src: "" } }
+  }
+  return await images[url]()
 }
